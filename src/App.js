f94b01013904211585d2e15bc27b50bd146eaf80
hecside <hzamoran@asu.edu>
2020-07-24 01:12:54 -0700

    added header navigation bar and links on bar to home page, shop page, and contact page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+
+//place the <Header/> outside the routes and switch like so
+//now the header will always dispay at the top of ever page 
+//in our app
+import Header from './components/header/header.component';
 
 
 
 function App() {
   return (
     <div>
+    <Header/> 
       <Switch>
         <Route   exact path='/' component={HomePage}/>
         <Route   exact path='/shop' component={ShopPage}/>
