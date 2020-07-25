d9f98afafc0b28ad85dd53edd8c39718f3e524d8
hecside <hzamoran@asu.edu>
2020-07-24 23:21:00 -0700

    Updates

---
+++ precache-manifest.4109ef3ea0969581d67611097c0a683c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0487a176a12c9a4b5076194e516bdbe",
+    "url": "/ecom-react-app/index.html"
+  },
+  {
+    "revision": "5f4f6b2942758b94858c",
+    "url": "/ecom-react-app/static/css/main.6f5de759.chunk.css"
+  },
+  {
+    "revision": "9425ff3144a8717be0e1",
+    "url": "/ecom-react-app/static/js/2.7f0cbf09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ecom-react-app/static/js/2.7f0cbf09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f4f6b2942758b94858c",
+    "url": "/ecom-react-app/static/js/main.efeeca70.chunk.js"
+  },
+  {
+    "revision": "7ea9bd3fb2891403fba4",
+    "url": "/ecom-react-app/static/js/runtime-main.fa6ee422.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecom-react-app/static/media/crown.ed28accb.svg"
+  }
+]);
